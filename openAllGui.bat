ced1bfeabbb3e2cca855c6746f11441ea92aae1e
Kyriakos Karoullas <kkaroullas@hfmarkets.local>
2025-08-13 10:22:34 +0300

    openAllGui modified

--- openAllGui.bat
+++ openAllGui.bat
@@ -1,10 +1,10 @@
 @echo off
-cd /d "C:\Users\kkaroullas\Documents\Kyriakos\Code\CourseCareers\BackEnd\brokerageGui"
+cd /d "C:\.....Path of the file openAllGui............."
 
 rem start server in a new window (keeps it open so you can see logs)
 start "brokerageGui server" cmd /k "npm run start"
 
 rem open the site in your default browser
-start "" "http://127.0.0.1:2626/"
+start "" "http://127.0.0.1:PORT/"
 
 pause
