664284ebb468f7702dc94561427c582e5c7d3ed5
Kyriakos Karoullas <kkaroullas@hfmarkets.local>
2025-08-13 10:20:09 +0300

    Remove Prisma migrations folder

--- prisma/migrations/20250812072817_initial/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "public"."Favorites" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "url" TEXT NOT NULL,
-
-    CONSTRAINT "Favorites_pkey" PRIMARY KEY ("id")
-);
